1b9e37a16885cf11f64fccd3ac6391f57368a5d3
Jennifer Bolanos <jbolanos707@gmail.com>
2015-06-05 09:31:36 -0700

    returns pizza type and size

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
-function Ticket(movieTitle, movieTime, ticketPrice) {
-  this.movieTitle = movieTitle;
-  this.movieTime = movieTime;
-  this.ticketPrice = ticketPrice;
-}
-
-Ticket.prototype.seniorDiscount = function() {
-  this.ticketPrice -= 6;
+function Pizza(pizzaType, pizzaSize, toppings) {
+  this.pizzaType = pizzaType;
+  this.pizzaSize = pizzaSize;
 };
-
-Ticket.prototype.matineeDiscount = function() {
-  this.ticketPrice -= 2;
-};
-
-$(document).ready(function(){
-
-$("form#movie1").submit(function(event) {
-  event.preventDefault();
-  var movieName = $('#movie1-title').text;
-  var movieTime = $('select#movie1-time option:selected').val();
-  var ticketPrice = 12;
-  debugger;
-
-  newTicket = new Ticket(movieName, movieTime, ticketPrice);
-  if ($("input#movie1-senior").checked){
-    newTicket.seniorDiscount();
-  }
-  else if (newTicket.movieTime ==="11:00am"){
-    newTicket.matineeDiscount();
-  }
-
-  $("#ticket-info").text("Your ticket for " + newTicket.movieName + " at " + newTicket.movieTime + "will cost $ " + newTicket.ticketPrice);
-
-});
-
-});
+//
+// Ticket.prototype.seniorDiscount = function() {
+//   this.ticketPrice -= 6;
+// };
+//
+// Ticket.prototype.matineeDiscount = function() {
+//   this.ticketPrice -= 2;
+// };
+//
+// $(document).ready(function(){
+//
+// $("form#movie1").submit(function(event) {
+//   event.preventDefault();
+//   var movieName = $('#movie1-title').text;
+//   var movieTime = $('select#movie1-time option:selected').val();
+//   var ticketPrice = 12;
+//   debugger;
+//
+//   newTicket = new Ticket(movieName, movieTime, ticketPrice);
+//   if ($("input#movie1-senior").checked){
+//     newTicket.seniorDiscount();
+//   }
+//   else if (newTicket.movieTime ==="11:00am"){
+//     newTicket.matineeDiscount();
+//   }
+//
+//   $("#ticket-info").text("Your ticket for " + newTicket.movieName + " at " + newTicket.movieTime + "will cost $ " + newTicket.ticketPrice);
+//
+// });
+//
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,7 @@
-describe('ticket', function(){
-  it("displays movie title and time", function(){
-    var newTicket = new Ticket("True Cost", "11:00am", 12);
-    expect(newTicket.movieTitle).to.equal("True Cost");
-    expect(newTicket.movieTime).to.equal("11:00am");
-    expect(newTicket.ticketPrice).to.equal(12);
-  });
-
-  it("discounts the ticket price for seniors", function() {
-    var newTicket = new Ticket("True Cost", "11:00am", 12);
-    newTicket.seniorDiscount();
-    expect(newTicket.ticketPrice).to.equal(6);
-  });
-
-  it("discounts the ticket price for matinee", function() {
-    var newTicket = new Ticket("True Cost", "11:00am", 12);
-    newTicket.matineeDiscount();
-    expect(newTicket.ticketPrice).to.equal(10);
+describe('Pizza', function(){
+  it("has type and size", function(){
+    var newPizza = new Pizza("cheese", "medium");
+    expect(newPizza.pizzaType).to.equal("cheese");
+    expect(newPizza.pizzaSize).to.equal("medium");
   });
 });
